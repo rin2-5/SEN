1a287d735cb1249f4fa73f65c68254c93ddc6654
rin2-5 <linjiax25@gmail.com>
2022-06-09 11:12:10 +0800

    Deleted Header

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from "./src/components/Header";
 
 export default function App() {
   return (
--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-import {Text} from "react-native";
-
-export default function Header() {
-    return <Text>Hello Friends!</Text>
-}
