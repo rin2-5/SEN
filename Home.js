11fce9c7a1cb39adbaa491afb168c11fc2bd12c8
Celine Chan <celine0242@gmail.com>
2022-06-12 16:28:20 +0800

    screens

--- Home.js
+++ Home.js
@@ -1,22 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
+// run "npm install @react-navigation/native @react-navigation/stack @react-navigation/bottom-tabs" in terminal
 
-export default function App() {
+function GetfoodScreen() {
+  const navigation = useNavigation();
+//Home page
   return (
-    <View style={styles.container}>
-      <Header />
-      <Text>Hello 'w' </Text>
-      <Text>How can I help you today</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center",backgroundColor: "#D3EBCD", }}>
+      <Text >Home</Text>
+      <Button
+        title="Order Food"
+        onPress={() => {
+          navigation.navigate("OrderFood.js");
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function GetfoodScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+      }}
+    >
+      <text>Getfoodscreen</text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function EventsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="EventsHome" component={EventsScreen} />
+      <Stack.Screen name="EventsSecondScreen" component={EventsSecondScreen} />
+    </Stack.Navigator>
+  );
+}
